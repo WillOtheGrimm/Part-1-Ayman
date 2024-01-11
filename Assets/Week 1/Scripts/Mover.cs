26f2c9c0c14b0003d228e3a954c6ffcaa2613368
WILLO_LAPTOP\ayman <ayman010905@gmail.com>
2024-01-11 09:40:15 -0500

    ask 2 b completed: tank firing a missile on keyboard input.

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {   
     public float speed = 5f;
+    public GameObject prefab;
+    public Transform barrel;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +21,12 @@ public class Mover : MonoBehaviour
     {
         float keyboardInput = Input.GetAxis("Horizontal");
         transform.Translate(keyboardInput * speed * Time.deltaTime, 0, 0);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, barrel.position, barrel.rotation);
+        }
+
     }
 }
