ad0f2868bd1970fb8d430261a7ad37e0c84c8968
WILLO_LAPTOP\ayman <ayman010905@gmail.com>
2024-01-17 13:59:49 -0500

    Player is moving on keyboard input, and collision was added to wall.

---
+++ Assets/Week 2/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody;
+
+    //To have a speed variable for player movement
+    public float speed = 5f;
+
+    Vector2 direction;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is to control input and movement 
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+       
+        rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+}
