5d053cec3e6f6724097fa2aa5be812d2e4c166d1
WILLOPC\Ayman010905 <ayman010905@gmail.com>
2024-01-29 22:43:10 -0500

    Task 2 done, added body segments. Added segment script for  rotation / distance from head. and made sure head can't rotate on it's own.

---
+++ Assets/Assignment/Script/SnakeBody.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBody : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    public SnakeHead head;
+    public Transform nextBodyPiece;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 changeInPosition = (transform.position - nextBodyPiece.position).normalized * head.segmentLength;
+
+        rigidbody.MovePosition((Vector2)nextBodyPiece.position + changeInPosition);
+
+
+
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward,nextBodyPiece.position - transform.position);
+        rigidbody.MoveRotation(targetRotation);
+
+    }
+
+
+}
--- Assets/Assignment/Script/SnakeHead.cs
+++ Assets/Assignment/Script/SnakeHead.cs
@@ -6,43 +6,85 @@ public class SnakeHead : MonoBehaviour
 {
 
     Rigidbody2D rigidbody;
-    
+
 
     float acceleration;
     float turnInput;
     public float movingSpeed = 200;
-    public float turningSpeed = 100;
+    public float turningSpeed = 300;
     public float maxSpeed = 200;
+    public float segmentLength = 1;
+    List<SnakeBody> bodySegments = new();
+    public SnakeBody bodyPrefab;
+    public float maxHeadAngle = 90;
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        AddBody();
+        AddBody();
+        AddBody();
+        AddBody();
+        AddBody();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
         turnInput = Input.GetAxis("Horizontal");
 
     }
 
     private void FixedUpdate()
     {
-        rigidbody.AddTorque(turnInput * - turningSpeed * Time.deltaTime);
+
+        if (bodySegments.Count > 0)
+        {
+            Vector3 distanceFromBody = transform.position - bodySegments[0].transform.position;
+            float headAngle = Vector3.Angle(distanceFromBody, transform.up);
+
+            if (headAngle <= maxHeadAngle)
+            {
+
+                rigidbody.AddTorque(turnInput * -turningSpeed * Time.deltaTime);
+            }
+        }
+
 
 
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
-            Vector2 force = transform.right  * movingSpeed * Time.deltaTime;
+            Vector2 force = transform.up * movingSpeed * Time.deltaTime;
             rigidbody.AddForce(force);
         }
 
 
     }
 
+    private void AddBody()
+    {
+
+        SnakeBody newSegment = Instantiate(bodyPrefab);
+        newSegment.head = this;
+
+        if (bodySegments.Count == 0)
+        {
+            newSegment.nextBodyPiece = transform;
+        }
+        else
+        {
+            newSegment.nextBodyPiece = bodySegments[bodySegments.Count - 1].transform;
+        }
+        bodySegments.Add(newSegment);
+        newSegment.transform.position = newSegment.nextBodyPiece.position - newSegment.nextBodyPiece.up * segmentLength;
+
+    }
 
 
 }
